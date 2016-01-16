bb8d0a6b2520c01ec4fed10045df249a559f9837
Mateusz Jabłoński <mjablonski92@gmail.com>
2016-01-16 17:45:29 +0100

    json

--- Percolation/Percolation/Calculator.cpp
+++ Percolation/Percolation/Calculator.cpp
@@ -1,5 +1,9 @@
 #include "Calculator.h"
-#include <iostream>
+
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
 Calculator::Calculator(std::size_t width, std::size_t height, double probability, GridType type, bool gravity) :
 	grid(width, height, probability, type, gravity)
 {
@@ -12,20 +16,59 @@ Calculator::~Calculator()
 std::string Calculator::calculate(std::size_t steps)
 {
 	using namespace boost::accumulators;
+	using namespace rapidjson;
 
 	accumulator_set<std::size_t, stats<tag::mean>> acc;
 
 	while (steps-- != 0)
 		acc(static_cast<std::size_t>(grid.percolate()));
 
-	return std::to_string(mean(acc));
+	Document doc(kObjectType);
+
+	doc.AddMember("data", mean(acc), doc.GetAllocator());
+
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	doc.Accept(writer);
+
+	return buffer.GetString();
 }
 
 std::string Calculator::simulate()
 {
-	grid.simulate();
+	using namespace rapidjson;
 
-	//TODO
+	const Grid::Simulation data = grid.simulate();
 
-	return "TODO";
+	Document doc(kObjectType);
+	auto &allocator = doc.GetAllocator();
+
+	Value simulation(kArrayType);
+
+	for (const Grid::SimulationStep &step : data)
+	{
+		Value step_data(kArrayType);
+
+		for (const Grid::SimulationRow &row : step)
+		{
+			Value row_value(kArrayType);
+
+			for (const uint8_t &value : row)
+			{
+				row_value.PushBack(value, allocator);
+			}
+
+			step_data.PushBack(row_value, allocator);
+		}
+
+		simulation.PushBack(step_data, allocator);
+	}
+
+	doc.AddMember("data", simulation, doc.GetAllocator());
+
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	doc.Accept(writer);
+
+	return buffer.GetString();
 }
